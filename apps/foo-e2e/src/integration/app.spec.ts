d91f80e260899dce0c63f671cdb67e1546c8727b
Kevin Anidjar <kevin.anidjar@consulting-for.edenred.com>
2022-05-24 22:02:00 +0200

    Add faker

--- apps/foo-e2e/src/integration/app.spec.ts
+++ apps/foo-e2e/src/integration/app.spec.ts
@@ -1,9 +1,11 @@
+import { faker } from '@faker-js/faker';
 import { getGreeting } from '../support/app.po';
 
 describe('foo', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
+    console.log(faker.address.buildingNumber());
     // Custom command example, see `../support/commands.ts` file
     cy.login('my-email@something.com', 'myPassword');
 
